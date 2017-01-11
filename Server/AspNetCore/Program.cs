9a1957469668b82dfdd9e33b5fba1636bcaacaa7
FabianGosebrink <info@fabian-gosebrink.com>
2017-01-11 07:44:47 +0100

    added command line possibility for vscode/dotnet cli

--- Server/AspNetCore/Program.cs
+++ Server/AspNetCore/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.IO;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 
 namespace AspNetWebapiCore
 {
@@ -7,12 +8,18 @@ namespace AspNetWebapiCore
     {
         public static void Main(string[] args)
         {
+            var config = new ConfigurationBuilder()
+                        .AddCommandLine(args)
+                        .Build();
+
             var host = new WebHostBuilder()
-                .UseKestrel()
+                .UseConfiguration(config)
                 .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseKestrel()
                 .UseIISIntegration()
                 .UseStartup<Startup>()
                 .Build();
+
 
             host.Run();
         }
